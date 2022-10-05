0cd8faad594a5855e0326cac4829953e1bed912f
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-10-04 20:55:56 -0500

    layout done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
@@ -9,18 +8,19 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        vueIndentScriptAndStyle: false,
+      },
+      { usePrettierrc: true },
+    ],
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
-    'vue/script-indent': [
-      'error',
-      1,
-      {
-        baseIndent: 2,
-        switchCase: 2,
-        ignores: [],
-      },
-    ],
   },
   env: {
     browser: true,
--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import HomePage from '@/views/HomePage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  {
+    path: '/character/:id',
+    name: 'Character',
+    component: () => import('@/views/CharacterDetails.vue'),
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
 export default router
